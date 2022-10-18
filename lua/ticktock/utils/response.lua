2e720d16c86dd2c1c9184060c7ced3bfba6e0041
0x00-ketsu <ketsu.chin.121@gmail.com>
2022-10-18 19:59:59 +0800

    feat: if plugin `nvim-notify` installed, use custom title for `vim.notify`

--- lua/ticktock/utils/response.lua
+++ lua/ticktock/utils/response.lua
@@ -1,17 +1,29 @@
+local config = require('ticktock.config')
+
+local ok, notify = pcall(require, 'notify')
+
 local M = {}
 
 ---Send success message to client
 ---
 ---@param message string
 M.success = function(message)
-  vim.notify(message)
+  if ok then
+    notify(message, 'info', {title = config.plugin_name})
+  else
+    vim.notify(message)
+  end
 end
 
 ---Send failed message to client
 ---
 ---@param message string
 M.failed = function(message)
-  vim.notify(message, vim.log.levels.ERROR)
+  if ok then
+    notify(message, 'error', {title = config.plugin_name})
+  else
+    vim.notify(message, vim.log.levels.ERROR)
+  end
 end
 
 return M
